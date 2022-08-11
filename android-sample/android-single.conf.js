cb153cb18362d1af0f5db8481215365136ef4940
Ka-v-i-ta <74174371+Ka-v-i-ta@users.noreply.github.com>
2022-08-11 11:59:44 +0530

    Update android-single.conf.js

--- android-sample/android-single.conf.js
+++ android-sample/android-single.conf.js
@@ -1,6 +1,6 @@
 exports.config = {
-  user: ${{secrets.LT_USERNAME}} || "YOUR_USERNAME",
-  key:  ${{secrets.LT_ACCESS_KEY}} || "YOUR_ACCESS_KEY",
+  user: secrets.LT_USERNAME || "YOUR_USERNAME",
+  key:  secrets.LT_ACCESS_KEY || "YOUR_ACCESS_KEY",
 
   updateJob: false,
   specs: ["specs/android-test.js"],
